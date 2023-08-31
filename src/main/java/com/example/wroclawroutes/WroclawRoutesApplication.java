ce3a609f513b26cd071ad4f3ca5f644029a3abb1
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2023-08-31 11:15:51 +0200

    Initial commit

---
+++ src/main/java/com/example/wroclawroutes/WroclawRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.wroclawroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WroclawRoutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WroclawRoutesApplication.class, args);
+    }
+
+}
